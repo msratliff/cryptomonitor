b56ee484a978c30b4e38d2f986e536fa8deef389
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-08-18 15:19:32 -0400

    Created a way to populate database

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,7 +1,22 @@
+
 class CurrenciesController < ApplicationController
   def index
   end
 
   def show
   end
+
+  def create
+    response = HTTParty.get('https://api.coinmarketcap.com/v1/ticker/')
+    @url = HTTParty.get('https://api.coinmarketcap.com/v1/ticker/')
+    @response = JSON.parse(@url.body)
+
+
+    @response.each do |coin|
+      @currency = Currency.find_or_create_by(name: coin["name"], symbol: coin["symbol"], price_0: coin["price_usd"])
+      if @currency.save
+      end
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'currencies/show'
 
+  get 'currencies/new' => 'currencies#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
